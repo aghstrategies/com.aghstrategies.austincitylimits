b3d32fa6fc00116f0716fa3427d36b64717fbf73
Alice Frumin <alice@aghstrategies.com>
2016-02-10 17:30:55 -0500

    get districts

--- CRM/Austincitylimits/Geo.php
+++ CRM/Austincitylimits/Geo.php
@@ -16,6 +16,14 @@ class CRM_Austincitylimits_Geo {
    */
   public $address = NULL;
 
+  /**
+   * build the object
+   *
+   * @param float $lat
+   *   latitude
+   * @param float $long
+   *   longitude
+   */
   public function __construct($lat, $long) {
     //load geoPHP library
     include_once 'packages/geoPHP/geoPHP.inc';
@@ -32,6 +40,24 @@ class CRM_Austincitylimits_Geo {
     $austinJson = file_get_contents(CRM_Core_Resources::singleton()->getUrl('com.aghstrategies.austincitylimits', 'CRM/Austincitylimits/Districts.geojson'));
     $alldistricts = geoPHP::load($austinJson, 'json');
     $this->districts = $alldistricts->getComponents();
+  }
+
+  /**
+   * finds the district of the address
+   *
+   * @return int
+   *   the district
+   */
+  public function getDistrict() {
+    foreach ($this->districts as $key => $district) {
+      $addressGeos = $address->geos();
+      if ($district->geos()->contains($addressGeos)) {
+        return $key + 1;
+      }
+    }
+    // Put it back into a geoPHP geometry
+    $geometry = geoPHP::geosToGeometry($geos_result);
+
   }
 
 }
